4098d59ff8f058cb1bc63cb7c62b827666ff5d43
Tom J Nowell <contact@tomjn.com>
2017-01-11 16:56:31 +0000

    filter what talks are shown in queries depending on who's asking

--- includes/core/poststatus.php
+++ includes/core/poststatus.php
@@ -29,6 +29,7 @@ if ( ! class_exists( 'WordCamp_Talks_Post_Status' ) ) :
 			// register the various hooks
 			add_action( 'admin_head-post.php', array( $this, 'set_publishing_actions' ) );
 			add_action( 'admin_head-post-new.php', array( $this, 'set_publishing_actions' ) );
+			add_action( 'pre_get_posts', array( $this, 'filter_talks' ) );
 		}
 
 		function set_publishing_actions() {
@@ -40,6 +41,40 @@ if ( ! class_exists( 'WordCamp_Talks_Post_Status' ) ) :
 					display: none;
 				}
 				</style>';
+			}
+		}
+
+		/**
+		 * Filter which talks are seen by who
+		 *
+		 * @param  \WP_Query $q the query being filtered
+		 * @return void       nothing, the query is passed by reference
+		 */
+		function filter_talks( \WP_Query $q ) {
+			if ( 'talks' === $q->get( 'post_type' ) ) {
+				/**
+				 * logged out users should only be able to see those talks that have been picked
+				 */
+				if ( ! is_user_logged_in() ) {
+					$q->set( 'post_status', 'selected' );
+					return;
+				}
+
+				// are we on the frontend?
+				if ( ! is_admin() ) {
+					/**
+					 * This use is either a speaker or someone selecting, so show more post statuses.
+					 * We wouldn't want to do this in the admin area, else trashed posts and
+					 * published/private posts would be impossible to see, which would be difficult
+					 * to debug
+					 */
+					$q->set( 'post_status', array( 'draft', 'rejected', 'selected', 'shortlisted' ) );
+				}
+
+				// If the user cannot select talks, they must be a speaker, only show talks belonging to them
+				if ( ! wct_user_can( 'select-talks' ) ) {
+					$q->set( 'author', get_current_user_id() );
+				}
 			}
 		}
 	}
