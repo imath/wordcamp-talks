d80587de7afe2e532931f49e9ad604e9676620dc
Tom J Nowell <contact@tomjn.com>
2017-04-12 18:47:00 +0100

    removes the post status state labels in the posts list for talks

--- includes/core/status/views/posts-list.php
+++ includes/core/status/views/posts-list.php
@@ -17,6 +17,7 @@ class Talk_Status_View_Posts_List {
 	 * @return void
 	 */
 	public function run() {
+		add_filter( 'display_post_states', '__return_false' );
 		add_action( 'admin_init', [ $this, 'admin_init' ] );
 		add_filter( 'views_edit-talks', [ $this, 'update_subsubsub' ] );
 	}
