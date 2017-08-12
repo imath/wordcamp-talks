75b03c116f058abb885ee242e59daa47ca5b0065
imath <math.viet@gmail.com>
2017-08-12 23:57:45 +0200

    Add a unit test to check if the JS is enqueued on self profile

--- tests/phpunit/testcases/users/functions.php
+++ tests/phpunit/testcases/users/functions.php
@@ -23,4 +23,18 @@ class WordCampTalkProposalsTest_Users_Functions extends WordCampTalkProposalsTes
 	public function test_wct_users_current_user_id() {
 		$this->assertTrue( (int) get_current_user_id() === (int) wct_users_current_user_id() );
 	}
+
+	public function test_wct_users_enqueue_scripts() {
+		$enqueued = wp_scripts()->queue;
+		wct_set_global( 'is_user', get_current_user_id() );
+
+		do_action( 'wp_enqueue_scripts' );
+
+		$script = reset( wp_scripts()->queue );
+
+		$this->assertTrue( isset( wp_scripts()->registered[ $script ] ) );
+
+		wct_set_global( 'is_user', false );
+		wp_scripts()->queue = $enqueued;
+	}
 }
